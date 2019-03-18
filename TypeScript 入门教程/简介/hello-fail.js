3b83147f9a81ac2af7e60edcdee46c275044764f
qilei <qiilee@126.com>
2019-03-18 09:52:23 +0800

    new: 简介

---
+++ TypeScript 入门教程/简介/hello-fail.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = [0, 1, 2];
+console.log(sayHello(user));
---
+++ TypeScript 入门教程/简介/hello-fail.ts
@@ -0,0 +1,6 @@
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+
+let user = [0, 1, 2];
+console.log(sayHello(user));
